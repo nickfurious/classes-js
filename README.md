9b10568fc2d7fdfac22da7ebb72979b5ed741b36
nickfurious <datfuriouskid@live.com>
2021-04-25 18:16:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# classes-js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/classes-js)
---
+++ index.js
@@ -0,0 +1,23 @@
+function catName(name) {
+  console.log("My cat's name is " + name);
+}
+
+catName("Tiger");
+
+/*
+The result of the code above is: "My cat's name is Tiger"
+*/
+
+catName("Chloe");
+
+function catName(name) {
+  console.log("My cat's name is " + name);
+}
+/*
+The result of the code above is: "My cat's name is Chloe"
+*/
+catname2("Nick");
+
+function catname2(name) {
+  console.log("My dog's name is " + name);
+}
